324bf6ea3bd0a7d56defdaf3141eb497ff04c8c2
romain <zzz>
2020-03-28 12:49:17 +0100

    Commentaire sur planningController

--- APIplanning/Controllers/planningController.cs
+++ APIplanning/Controllers/planningController.cs
@@ -10,6 +10,7 @@ namespace APIplanning.Controllers
 {
     public class planningController : ApiController
     {
+        //Permet de créer une séance.
         [HttpPost]
         public HttpResponseMessage AddSeance([FromBody] Seance s)
         {
@@ -22,6 +23,8 @@ namespace APIplanning.Controllers
             else
                 return Request.CreateResponse(HttpStatusCode.BadRequest, s);
         }
+
+        //recupere une proposition de seance selon son id (id de seance).
         [HttpGet]
         public List<proposer> GetById(int id)
         {
@@ -29,6 +32,8 @@ namespace APIplanning.Controllers
             List<proposer> LesProposerParSeance = dao.getProposerSeance(id);
             return LesProposerParSeance;
         }
+
+        //Verifie une tentative de connexion.
         [HttpGet]
         public string verifMDP(string email , string mdp)
         {
@@ -37,7 +42,7 @@ namespace APIplanning.Controllers
             return connection;
         }
         
-
+        //recupere toute les seance.
         [HttpGet]
         public IEnumerable<Seance> GetAll()
         {
@@ -46,14 +51,17 @@ namespace APIplanning.Controllers
             return lesSeances.ToList();
         }
 
+        //Supprime une seance
         [HttpDelete]
-        public string DeleteContact(int id)
+        public string DeleteSeance(int id)
         {
             SeanceDAO dao = new SeanceDAO();
             dao.deleteSeance(id);
             return "Contact supprimé id " + id;
         }
+        
 
+        //Supprime une proposition d'un moniteur sur une seance
         [HttpDelete]
         public string DeleteProposer(int idSeance , int idMoniteur)
         {
@@ -62,6 +70,7 @@ namespace APIplanning.Controllers
             return "La Seance " + idSeance + "n'a plus la proposition de ce moniteur" ;
         }
 
+        //ajouter une proposition sur une seance
         [HttpPost]
         public string insertProposer(int id_seance,int moniteur)
         {
@@ -70,12 +79,14 @@ namespace APIplanning.Controllers
             return "Séance ajouté";
         }
 
+        /*
+         a supprimer inutile (je crois ) 
         [HttpPut]
         public string UpdateContact(string Name, int Id)
         {
             SeanceDAO dao = new SeanceDAO();
             dao.updateSeance(Id, Name);
             return "Mise à jour du contact avec le nom " + Name + " and Id " + Id;
-        }
+        }*/
     }
 }
